68b2e217dd0e3dd64441cef5f8059e36394b94e9
José Silva <jose.silva@primaverabss.com>
2018-04-19 11:37:43 +0100

    Added sample to create an Item and add a Sales Item extension

---
+++ code-samples/csharp-itemextensions-sample/csharp-itemextensions-sample/ConsoleHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ItemExtensions
+{
+    internal static class ConsoleHelper
+    {
+        #region Internal Methods
+
+        internal static void WriteErrorLine(string message)
+        {
+            ConsoleColor color = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ForegroundColor = color;
+        }
+
+        #endregion
+    }
+}
